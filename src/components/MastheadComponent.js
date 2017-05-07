3d733050862478923c50ab53cda1a5356e34ec29
alecholmez <alecholmez@me.com>
2017-05-07 00:50:19 -0400

    enlarged masthead

--- src/components/MastheadComponent.js
+++ src/components/MastheadComponent.js
@@ -8,13 +8,13 @@ let mastheadBackground = require('../images/placeholder.jpg');
 
 const mastheadBackgroundStyle = {
     backgroundImage: 'url(' + mastheadBackground + ')',
-    backgroundPosition: '30% 30%'
+    backgroundPosition: '50% 20%'
 };
 
 class MastheadComponent extends React.Component {
   render() {
     return (
-      <div className="uk-section uk-section-large uk-section-media uk-light uk-background-cover uk-text-center" style={mastheadBackgroundStyle}>
+      <div className="uk-section uk-section-xlarge uk-section-media uk-light uk-background-cover uk-text-center" style={mastheadBackgroundStyle}>
         <h1 className="uk-heading-hero light-weight">Jack Bischoff</h1>
       </div>
     );
