0d49e35c46db1bc92813c6f5a41d7acc72079c25
Jack Bischoff <Jack Bischoff>
2017-05-07 00:43:08 -0400

    Expanded Story section

--- src/components/StoryComponent.js
+++ src/components/StoryComponent.js
@@ -9,7 +9,10 @@ class StoryComponent extends React.Component {
     return (
       <div className= "story uk-section uk-section-medium uk-section-muted">
         <div className="uk-container">
-          <h1 className= "uk-margin-large-left">My Story</h1>
+          <h1 className= "uk-margin-large-left">Who Am I?</h1>
+          <p className="uk-margin-large-left uk-margin-large-right">
+              I am Lorem ipusm dolor sit amet, consectetur adipiscing elit. Fusce sed consectetur neque. Vestibulum sed sem nulla. Aenean auctor velit at sapien sollicitudin, eu porta mauris condimentum. Integer ut elementum purus, ac aliquam nisi. Praesent tristique risus enim, sed ultrices ex blandit eget. Praesent sed bibendum ex. Suspendisse commodo elit quis sem scelerisque efficitur.
+          </p>
         </div>
       </div>
     );
