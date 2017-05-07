d13ccb82a8a9ef1b7098094fbd179ae407117794
alecholmez <alecholmez@me.com>
2017-05-07 00:21:52 -0400

    changed colors. added links

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -7,26 +7,26 @@ require('styles//Footer.css');
 class FooterComponent extends React.Component {
   render() {
     return (
-        <div className="uk-section uk-section-muted">
+        <div className="uk-section space-grey uk-light">
           <div className="uk-container">
             <div className="uk-flex-center uk-text-center uk-child-width-1-3 uk-grid-divider" data-uk-grid>
               <div>
                   <h3>Code</h3>
                   <ul className="uk-list">
-                      <li><a className="uk-button uk-button-text uk-text-capitalize" href="">My Github</a></li>
+                      <li><a className="uk-button uk-button-text uk-text-capitalize" href="https://github.com/jack-bischoff"><span data-uk-icon="icon: github"></span> My Github</a></li>
+                      <li><a className="uk-button uk-button-text uk-text-capitalize" href="https://getuikit.com"><span data-uk-icon="icon: uikit"></span> Built With UIKit</a></li>
                   </ul>
               </div>
               <div>
                   <h3>Social Media</h3>
                   <ul className="uk-list">
-                      <li><a className="uk-button uk-button-text uk-text-capitalize" href="">Facebook</a></li>
-                      <li><a className="uk-button uk-button-text uk-text-capitalize" href="">Twitter</a></li>
+                      <li><a className="uk-button uk-button-text uk-text-capitalize" href="https://twitter.com/Mr_Foots"><span data-uk-icon="icon: twitter"></span> Twitter</a></li>
                   </ul>
               </div>
               <div>
                   <h3>Academic Writing</h3>
                   <ul className="uk-list">
-                      <li><a className="uk-button uk-button-text uk-text-capitalize" href="">Papers</a></li>
+                      <li><a className="uk-button uk-button-text uk-text-capitalize" href=""><span data-uk-icon="icon: file-edit"></span> Papers</a></li>
                   </ul>
               </div>
             </div>
--- src/components/MastheadComponent.js
+++ src/components/MastheadComponent.js
@@ -7,7 +7,7 @@ require('styles/Masthead.css');
 let mastheadBackground = require('../images/MastheadBackground.jpg');
 
 const mastheadBackgroundStyle = {
-    backgroundImage: 'url(' + mastheadBackground + ')',
+    backgroundImage: 'url(' + mastheadBackground + ')'
 };
 
 class MastheadComponent extends React.Component {
@@ -15,6 +15,7 @@ class MastheadComponent extends React.Component {
     return (
       <div className="uk-section uk-section-large uk-section-media uk-light uk-background-cover uk-text-center" style={mastheadBackgroundStyle}>
         <h1 className="uk-heading-hero light-weight">Jack Bischoff</h1>
+        <p className="uk-text-lead">"Dude, do me a solid... and swap those droids"</p>
       </div>
     );
   }
